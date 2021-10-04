eb16160148477724a1f54e9cc3c9774c969aa5ac
jungking <immenige2@gmail.com>
2021-10-04 21:01:55 +0900

    fix : change settings

---
+++ src/main/java/com/example/i_shopping/IShoppingApplication.java
@@ -0,0 +1,13 @@
+package com.example.i_shopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IShoppingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IShoppingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/i_shopping/IShoppingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.i_shopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IShoppingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
