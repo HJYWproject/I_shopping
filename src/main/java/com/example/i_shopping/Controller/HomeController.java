1880bb837b2d55b7e587ba5e1786b70ec626f592
jungking <immenige2@gmail.com>
2021-10-06 14:51:46 +0900

    append : a href signup, login, add controller

--- src/main/java/com/example/i_shopping/Controller/HomeController.java
+++ src/main/java/com/example/i_shopping/Controller/HomeController.java
@@ -6,8 +6,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-    @GetMapping
-    public String mainpage(){
+    @GetMapping("/")     // 메인 페이지 Controller
+    public String MainPage(){
         return "main";
+    }
+
+    @GetMapping("/login")     // 로그인 페이지 Controller
+    public String LoginPage(){
+        return "login";
+    }
+
+    @GetMapping("/signup")     // 회원가입 페이지 Controller
+    public String SignupPage(){
+        return "signup";
     }
 }
