9cae8eba13a990dfdbfc0231c8822a41ed7d012f
jungking <immenige2@gmail.com>
2021-10-08 20:53:30 +0900

    append : login

--- src/main/java/com/example/i_shopping/Controller/UserController.java
+++ src/main/java/com/example/i_shopping/Controller/UserController.java
@@ -17,17 +17,30 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class UserController {
 
     private final AccountService accountService;
+
     @PostMapping("/signup")
-    public String createUser(AccountForm form) throws Exception{
+    public String createUser(AccountForm form) throws Exception {
         try {
             accountService.findByUsername((form.getUsername()));
             accountService.createUser(form);
             System.out.println("회원가입 완료");
             return "redirect:/login";        // 성공 시, 로그인 페이지로
-        }catch(Exception e){
+        } catch (Exception e) {
             System.out.println("회원가입 실패");
             return "redirect:/signup";
         }
     }
-}
 
+
+    @PostMapping("/login")
+    public String LoginUser(AccountForm form) throws Exception {
+        try {
+            accountService.findByUsername((form.getUsername()));
+            System.out.println("로그인 완료");
+            return "redirect:/";        // 성공 시, 로그인 페이지로
+        } catch (Exception e) {
+            System.out.println("로그인 실패");
+            return "redirect:/login";
+        }
+    }
+}
