8255e05ffcf7adc96890ebdc92cee232992b5182
jungking <immenige2@gmail.com>
2021-11-09 16:12:56 +0900

    append : bascket form

--- src/main/java/com/example/i_shopping/Shopping/Controller/ShopController.java
+++ src/main/java/com/example/i_shopping/Shopping/Controller/ShopController.java
@@ -6,23 +6,4 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ShopController {
 
-    @GetMapping("/posthome")
-    public String mainpost(){
-        return "/post/posthome";
-    }
-
-    @GetMapping("/freepostpage")
-    public String freepost(){
-        return "/post/freepostpage";
-    }
-
-    @GetMapping("/faqpostpage")
-    public String faqpost(){
-        return "/post/faqpostpage";
-    }
-
-    @GetMapping("/questionpostpage")
-    public String questionpost(){
-        return "/post/questionpostpage";
-    }
 }
