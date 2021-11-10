15f4b82ddfbc5ed04576e37e8247ca8dd91862fd
jungking <immenige2@gmail.com>
2021-11-10 18:09:36 +0900

    append : shopping page form

--- src/main/java/com/example/i_shopping/Shopping/Controller/ShopController.java
+++ src/main/java/com/example/i_shopping/Shopping/Controller/ShopController.java
@@ -6,4 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ShopController {
 
+    @GetMapping("/shopping_main")
+    public String basket_page(){
+        return "/shop/shopping_main";
+    }
 }
